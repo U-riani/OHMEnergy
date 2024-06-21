bb0e3ca18ea725d04137f684b1fb2f04c89058c8
U-riani <sandro.papi@yahoo.com>
2024-06-21 14:34:16 +0400

    modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ import {
   faInstagram,
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
-// import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <Container fluid className="">
+    <Container fluid className="bg-secondary-subtle">
       <Row className="bg-body-secondary py-4">
         <Col sm={12} className="text-center">
           <h3>Follow us</h3>
@@ -29,31 +29,53 @@ const Footer = () => {
           </div>
         </Col>
       </Row>
-      <Row className="py-4 border-bottom px-5 ">
-        <Col sm={4}>
-          <div className="footer-text-inner-container">
-            <h4>Addres</h4>
+      <Row className="py-4 border-bottom px-5 footer-text-container">
+        <Col
+          md={4}
+          lg={3}
+          className="footer-text-col ps-5 px-md-0 mb-4 mb-md-0"
+        >
+          <div className="footer-text-inner-container ">
+            <h4 className="pb-2">Addres</h4>
+            <br />
             <p>Sulkhan Tsintsadze st. 22</p>
+            <br />
             <p>Tbilisi, Georgia</p>
           </div>
         </Col>
-        <Col sm={4}>
+        <Col
+          md={4}
+          lg={3}
+          className="footer-text-col ps-5 px-md-0 mb-4 mb-md-0"
+        >
           <div className="footer-text-inner-container">
-            <h4>Working hours</h4>
+            <h4 className="pb-2">Working hours</h4>
+            <br />
             <p>Mon - Fri, 09:00 - 18:00</p>
+            <br />
             <p>Saturday, 10:00 - 17:00</p>
           </div>
         </Col>
-        <Col sm={4}>
+        <Col md={4} lg={3} className="footer-text-col ps-5 px-md-0 ">
           <div className="footer-text-inner-container">
-            <h4>Contact</h4>
-            <p>+995 598 598 598</p>
-            <p>ohmenergy@info.ge</p>
+            <h4 className="pb-2">Contact</h4>
+            <br />
+            <div className="number-container footer-info-containers">
+              <FontAwesomeIcon icon={faPhone} />
+              <p className="fst-italic ps-3">+995 598 598 598</p>
+            </div>
+            <br />
+            <div className="mail-container footer-info-containers">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <p className="fst-italic ps-3">ohmenergy@info.ge</p>
+            </div>
           </div>
         </Col>
       </Row>
       <Row className="py-4">
-        <p className="text-center">© Ohm Energy Solutions 2024</p>
+        <p className="text-center">
+          © <span className="text-warning">Ohm Energy</span> Solutions 2024
+        </p>
       </Row>
     </Container>
   );
