9d32dcebc5489479761810fcf064f9f79e1d5dfe
U-riani <sandro.papi@yahoo.com>
2024-07-19 21:02:10 +0400

    add send email functionality

--- src/components/ContactsFormComponent.js
+++ src/components/ContactsFormComponent.js
@@ -1,72 +1,138 @@
-import React from "react";
-import { FloatingLabel, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { FloatingLabel, Form, Button } from "react-bootstrap";
 
 const ContactsFormComponent = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    companyName: "",
+    email: "",
+    countryCode: "+995",
+    phoneNumber: "",
+    comments: "",
+  });
+  console.log({ ...formData });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.text()) // Expecting plain text
+      .then((data) => {
+        alert(data); // Alerting the response text
+        if (data === "Email sent successfully") {
+          setFormData({
+            name: "",
+            companyName: "",
+            email: "",
+            countryCode: "+995",
+            phoneNumber: "",
+            comments: "",
+          });
+        }
+      })
+      .catch((error) => {
+        alert("Error sending email");
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className="contact-page-forms-inner-container">
-      <div className="contact-page-form-name-company-name-input-container">
-        <Form.Floating className="mb-3 contact-page-from-input contact-page-from-name-input">
-          <Form.Control
-            id="floatingInputCustom"
-            type="text"
-            placeholder="name"
-          />
-          <label htmlFor="floatingInputCustom">name</label>
-        </Form.Floating>
-        <Form.Floating className="mb-3 contact-page-from-input contact-page-from-company-name-input">
-          <Form.Control
-            id="floatingInputCustom"
-            type="text"
-            placeholder="company name"
-          />
-          <label htmlFor="floatingInputCustom">company name</label>
-        </Form.Floating>
-      </div>
-      <div className="contact-page-form-email-phone-number-input-container">
-        <Form.Floating className="mb-3 contact-page-from-input contact-page-from-email-input">
-          <Form.Control
-            id="floatingInputCustom"
-            type="email"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="floatingInputCustom">Email address</label>
-        </Form.Floating>
-        <div className="mb-3 form-floating-container contact-page-form-number-container">
-          <Form.Floating className="contact-page-from-input contact-page-form-number-start-input-container">
-            <Form.Select
-              aria-label="Floating label select example"
-              id="floatingInputCustom"
-              className="contact-page-form-phone-number-start"
-            >
-              {/* <option>+995</option> */}
-              <option value="1">+995</option>
-              <option value="2">+324</option>
-              <option value="3">+342</option>
-            </Form.Select>
-            <label htmlFor="floatingInputCustom">county code</label>
-          </Form.Floating>
-          <Form.Floating className="contact-page-from-input contact-page-form-number-input-container">
+      <Form onSubmit={handleSubmit}>
+        {/* Form Fields */}
+        <div className="contact-page-form-name-company-name-input-container">
+          <Form.Floating className="mb-3 contact-page-from-input contact-page-from-name-input">
             <Form.Control
-              id="floatingnumber"
-              type="tel"
-              placeholder="phone number"
-              className="contact-page-from-input contact-page-form-number-input"
+              id="floatingInputCustom"
+              type="text"
+              name="name"
+              placeholder="name"
+              value={formData.name}
+              onChange={handleChange}
             />
-            <label htmlFor="floatingnumber">phone number</label>
+            <label htmlFor="floatingInputCustom">name</label>
+          </Form.Floating>
+          <Form.Floating className="mb-3 contact-page-from-input contact-page-from-company-name-input">
+            <Form.Control
+              id="floatingInputCustom"
+              type="text"
+              name="companyName"
+              placeholder="company name"
+              value={formData.companyName}
+              onChange={handleChange}
+            />
+            <label htmlFor="floatingInputCustom">company name</label>
           </Form.Floating>
         </div>
-      </div>
-      <FloatingLabel
-        controlId="floatingTextarea2"
-        label="Comments"
-        className="mb-3 contact-page-from-input contact-page-from-comment-input"
-      >
-        <Form.Control
-          as="textarea"
-          placeholder="Leave a comment here"
-          style={{ height: "100px" }}
-        />
-      </FloatingLabel>
+        <div className="contact-page-form-email-phone-number-input-container">
+          <Form.Floating className="mb-3 contact-page-from-input contact-page-from-email-input">
+            <Form.Control
+              id="floatingInputCustom"
+              type="email"
+              name="email"
+              placeholder="name@example.com"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <label htmlFor="floatingInputCustom">Email address</label>
+          </Form.Floating>
+          <div className="mb-3 form-floating-container contact-page-form-number-container">
+            <Form.Floating className="contact-page-from-input contact-page-form-number-start-input-container">
+              <Form.Select
+                aria-label="Floating label select example"
+                id="floatingInputCustom"
+                className="contact-page-form-phone-number-start"
+                name="countryCode"
+                value={formData.countryCode}
+                onChange={handleChange}
+              >
+                <option value="+995">+995</option>
+                <option value="+324">+324</option>
+                <option value="+342">+342</option>
+              </Form.Select>
+              <label htmlFor="floatingInputCustom">country code</label>
+            </Form.Floating>
+            <Form.Floating className="contact-page-from-input contact-page-form-number-input-container">
+              <Form.Control
+                id="floatingnumber"
+                type="tel"
+                name="phoneNumber"
+                placeholder="phone number"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                className="contact-page-from-input contact-page-form-number-input"
+              />
+              <label htmlFor="floatingnumber">phone number</label>
+            </Form.Floating>
+          </div>
+        </div>
+        <FloatingLabel
+          controlId="floatingTextarea2"
+          label="Comments"
+          className="mb-3 contact-page-from-input contact-page-from-comment-input"
+        >
+          <Form.Control
+            as="textarea"
+            name="comments"
+            placeholder="Leave a comment here"
+            style={{ height: "100px" }}
+            value={formData.comments}
+            onChange={handleChange}
+          />
+        </FloatingLabel>
+        <Button className="contact-page-submit-button" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
